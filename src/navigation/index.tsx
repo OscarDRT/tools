531f6b562f1513e4084afae61329afb51069a706
OscarDRT <oscarnetworkingpro@gmail.com>
2022-06-18 16:37:57 -0500

    feat: add navigation stack

---
+++ src/navigation/index.tsx
@@ -0,0 +1,63 @@
+/**
+ * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
+ * https://reactnavigation.org/docs/getting-started
+ *
+ */
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {HomeScreen} from '../screens/HomeScreen';
+
+//import {HomeScreen} from '../screens/HomeScreen';
+
+//import LinkingConfiguration from './LinkingConfiguration';
+//import {TimerScreen} from '../screens/TimerScreen';
+//import {useBackHandler} from '@react-native-community/hooks';
+
+export default function Navigation() {
+  /*  useBackHandler(() => {
+    return true;
+  }); */
+
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+/*
+ /**
+  * A root stack navigator is often used for displaying modals on top of all other content.
+  * https://reactnavigation.org/docs/modal
+  */
+const Stack = createNativeStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      {/* <Stack.Screen name="TimerScreen" component={TimerScreen} /> */}
+    </Stack.Navigator>
+  );
+};
+
+/**
+  * A bottom tab navigator displays tab buttons on the bottom of the display to switch screens.
+  * https://reactnavigation.org/docs/bottom-tab-navigator
+  *
+ const BottomTab = createBottomTabNavigator<RootTabParamList>();
+ function BottomTabNavigator() {
+   const colorScheme = useColorScheme();
+   return (
+     <BottomTab.Navigator
+       initialRouteName="TabOne"
+       screenOptions={{
+         tabBarActiveTintColor: Colors[colorScheme].tint,
+       }}>
+
+     </BottomTab.Navigator>
+   );
+ }
+  */
---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {Text, View} from 'react-native';
+import {RootStackScreenProps} from '../../../types';
+import {TextInput} from 'react-native-paper';
+import {RFValue} from 'react-native-responsive-fontsize';
+
+export const HomeScreen = ({
+  navigation,
+}: RootStackScreenProps<'HomeScreen'>) => {
+  const [text, setText] = React.useState('');
+
+  const goToTimerScreen = () => {
+    console.log(text);
+    navigation.navigate('TimerScreen', {title: text});
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>¿En qué deseas enfocar tu tiempo?</Text>
+      <TextInput
+        /*  activeUnderlineColor={theme.dark.background}
+        underlineColor={theme.dark.background}
+        selectionColor={theme.dark.background} */
+        onSubmitEditing={goToTimerScreen}
+        autoFocus
+        value={text}
+        onChangeText={v => setText(v)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+    //backgroundColor: theme.dark.background,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: RFValue(20),
+    fontWeight: 'bold',
+    marginBottom: 16,
+    //color: theme.dark.primaryText,
+  },
+});
