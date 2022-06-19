69969b61a0fa18110a31e52e26d0db1ce4dddc69
OscarDRT <oscarnetworkingpro@gmail.com>
2022-06-19 15:32:27 -0500

    feat: Switch for theme

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -8,10 +8,15 @@ import {lightTheme, Theme, darkTheme} from './theme';
 
 interface ThemeProviderProps {
   children?: React.ReactElement;
+  isDarkTheme: boolean;
 }
 
 export const useTheme = () => useReTheme<Theme>();
 
-export const ThemeProvider = ({children}: ThemeProviderProps) => {
-  return <ThemeProviderRS theme={darkTheme}>{children}</ThemeProviderRS>;
+export const ThemeProvider = ({children, isDarkTheme}: ThemeProviderProps) => {
+  return (
+    <ThemeProviderRS theme={isDarkTheme ? darkTheme : lightTheme}>
+      {children}
+    </ThemeProviderRS>
+  );
 };
